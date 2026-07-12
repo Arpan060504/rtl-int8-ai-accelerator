6a87d26f05277a6ac733d38898b0842067bd2ef1
Arpan Chandra <arpanchaandra@gmail.com>
2026-07-12 23:18:01 +0530

    Add files via upload

--- rtl/v0_1_mac/v1_signed_mac_pe.v
+++ rtl/v0_1_mac/v1_signed_mac_pe.v
@@ -1,1 +1,15 @@
+module v1_signed_mac_pe(clk ,reset , clear_acc , enable , a , b , acc_out);
+input clk , reset , clear_acc , enable;
+input signed [7:0] a , b ;
+output reg signed [31:0] acc_out;
 
+always @(posedge clk ) 
+begin
+    if(reset)
+        acc_out <= 0;
+    else if(clear_acc)
+        acc_out <= 0;
+    else if(enable)
+        acc_out <= acc_out + a*b;    
+end
+endmodule
