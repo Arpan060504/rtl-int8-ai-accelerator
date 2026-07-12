eeeed735f05a97f4cded1156bd75dad84fab4e83
Arpan Chandra <arpanchaandra@gmail.com>
2026-07-12 23:21:51 +0530

    Add files via upload

--- rtl/v0_4_systolic_pe/v4_systolic_pe.v
+++ rtl/v0_4_systolic_pe/v4_systolic_pe.v
@@ -1,1 +1,30 @@
+module v4_systolic_pe(clk , reset , clear , enable , a_in , b_in 
+, a_out , b_out , acc_out );
 
+input clk , reset , clear , enable ;
+input signed [7:0] a_in , b_in ;
+output reg signed [7:0] a_out , b_out;
+output signed [31:0] acc_out;
+
+always @(posedge clk)
+begin
+    if(reset)
+        begin
+            a_out <= 0;
+            b_out <= 0;
+        end
+end
+
+v1_signed_mac_pe pe (.clk(clk) ,
+                    .reset(reset) , .clear_acc(clear) , .enable(enable) 
+                    , .a(a_in) , .b(b_in) , .acc_out(acc_out));
+
+always @(posedge clk)
+begin
+    if(enable)
+        begin
+            a_out <= a_in;
+            b_out <= b_in;
+        end   
+end
+endmodule
